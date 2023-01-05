c2aafe6766b7d2836557b07d62fb5fba25a6a137
MrCronkite <vladshima99@gmail.com>
2023-01-05 20:23:06 +0300

    optinal

--- Optional Types.playground/Contents.swift
+++ Optional Types.playground/Contents.swift
@@ -1,7 +1,16 @@
 
 
 
-let somNumber = "asdas"
+let somNumber = "111"
 let convertNumber = Int(somNumber)
 
-print(convertNumber!)
+print(convertNumber ?? "ошибка")
+
+
+if let convertNumber = Int(somNumber){
+    print(convertNumber)
+} else {
+    print("ошибка")
+}
+
+guard let convertNumber1 = Int(somNumber) else {print("ошибка")}
