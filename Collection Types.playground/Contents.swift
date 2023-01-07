717503d99149a21ef934aa2b6b40e2cdca817f02
MrCronkite <vladshima99@gmail.com>
2023-01-07 21:35:17 +0300

    collection types

--- Collection Types.playground/Contents.swift
+++ Collection Types.playground/Contents.swift
@@ -2,10 +2,18 @@
 
 var arrayInt: [Int] = [4, 6, 7, 6, 9]
 
-arrayInt.append(3)	
+arrayInt.append(3)
+arrayInt.insert(15, at: 4)
 
-print(arrayInt)
+arrayInt.remove(at: 2)
+
+print("\(arrayInt), количество элементов в масиве \(arrayInt.count)")
+
+print(arrayInt[0],  arrayInt[1...4])
 
 
+for i in arrayInt {
+    print(i)
+}
 
 
