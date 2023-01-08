fb1e9d10cbaf6e0e6cf80c3dfb76be01f6ede56a
MrCronkite <vladshima99@gmail.com>
2023-01-08 21:48:24 +0300

    control flow

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -1,57 +1,10 @@
 
+let names = ["Kolia", "Roma", "Ignat", "Ruslan"]
 
-//обычная функция
-func getName(){
-    print("Vlad")
+func backward(_ s1: String, _ s2: String) -> Bool{
+    return s1 > s2
 }
 
-getName()
+let reversName = names.sorted(by: backward)
 
-
-//функция с параметрами
-func getNameParam(name item: String){
-    print(item)
-}
-
-getNameParam(name: "Vlad")
-
-
-// функция возвращающаяя значение
-func getNameParam_(name item: String) -> String{
-    return "hello \(item)"
-}
-
-print(getNameParam_(name: "vlad"))
-
-
-//сквозные параметры
-func swapNumber(_ a: inout Int, _ b: inout Int){
-    let tempNUmber = a
-    a = b
-    b = tempNUmber
-}
-
-var someNumber = 32
-var anotherNumber = 78
-swapNumber(&someNumber, &anotherNumber)
-print(someNumber, anotherNumber)
-
-
-//функциональный тип
-
-func addNumber(_ a: Int, _ b: Int) -> Int {
-    return a + b
-}
-
-var mathcFunc: (Int, Int) -> Int = addNumber
-
-print(mathcFunc(2, 5))
-
-
-//как параметр
-
-func printNumber(_ matchFunction: (Int, Int) -> Int, _ a: Int, _ b: Int){
-    print(matchFunction(a, b))
-}
-
-printNumber(addNumber, 10, 12)
+print(reversName)
---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,57 @@
+
+
+//обычная функция
+func getName(){
+    print("Vlad")
+}
+
+getName()
+
+
+//функция с параметрами
+func getNameParam(name item: String){
+    print(item)
+}
+
+getNameParam(name: "Vlad")
+
+
+// функция возвращающаяя значение
+func getNameParam_(name item: String) -> String{
+    return "hello \(item)"
+}
+
+print(getNameParam_(name: "vlad"))
+
+
+//сквозные параметры
+func swapNumber(_ a: inout Int, _ b: inout Int){
+    let tempNUmber = a
+    a = b
+    b = tempNUmber
+}
+
+var someNumber = 32
+var anotherNumber = 78
+swapNumber(&someNumber, &anotherNumber)
+print(someNumber, anotherNumber)
+
+
+//функциональный тип
+
+func addNumber(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+var mathcFunc: (Int, Int) -> Int = addNumber
+
+print(mathcFunc(2, 5))
+
+
+//как параметр
+
+func printNumber(_ matchFunction: (Int, Int) -> Int, _ a: Int, _ b: Int){
+    print(matchFunction(a, b))
+}
+
+printNumber(addNumber, 10, 12)
