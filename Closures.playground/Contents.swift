04a2fc610bea7e96ae9a002148d88c60933d692b
MrCronkite <vladshima99@gmail.com>
2023-01-08 21:57:36 +0300

    control flow

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -7,4 +7,12 @@ func backward(_ s1: String, _ s2: String) -> Bool{
 
 let reversName = names.sorted(by: { (s1: String, s2: String) -> Bool in return s1 > s2})
 
+let reversName_1 = names.sorted(by: {(s1, s2) -> Bool in s1 > s2 })
+
+let reversName_2 = names.sorted { s2, s1 in s1 > s2 }
+
+print(reversName_2)
+
+print(reversName_1)
+
 print(reversName)
