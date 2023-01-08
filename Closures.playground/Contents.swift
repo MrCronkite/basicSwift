b659e35ddb781d1b0ce02d69b0b18761fbebb87b
MrCronkite <vladshima99@gmail.com>
2023-01-08 21:25:09 +0300

    control flow

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -1,23 +1,30 @@
 
 
+//обычная функция
 func getName(){
     print("Vlad")
 }
 
 getName()
 
+
+//функция с параметрами
 func getNameParam(name item: String){
     print(item)
 }
 
 getNameParam(name: "Vlad")
 
+
+// функция возвращающаяя значение
 func getNameParam_(name item: String) -> String{
     return "hello \(item)"
 }
 
 print(getNameParam_(name: "vlad"))
 
+
+//сквозные параметры
 func swapNumber(_ a: inout Int, _ b: inout Int){
     let tempNUmber = a
     a = b
@@ -28,3 +35,17 @@ var someNumber = 32
 var anotherNumber = 78
 swapNumber(&someNumber, &anotherNumber)
 print(someNumber, anotherNumber)
+
+
+//функциональный тип
+
+func addNumber(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+var mathcFunc: (Int, Int) -> Int = addNumber
+
+print(mathcFunc(2, 5))
+
+
+//как параметр 
