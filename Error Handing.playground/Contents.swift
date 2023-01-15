16394a6aac1cd6771b4c7a9e007ba24ba0365012
MrCronkite <vladshima99@gmail.com>
2023-01-15 21:23:05 +0300

    control flow

--- Error Handing.playground/Contents.swift
+++ Error Handing.playground/Contents.swift
@@ -5,3 +5,29 @@ enum VendingMachineError: Error {
     case insufficientFound(coinsNeed: Int)
     case outOfStock
 }
+
+throw VendingMachineError.insufficientFound(coinsNeed: 4)
+
+
+struct Item {
+    var price: Int
+    var count: Int
+}
+
+class VendingMachine {
+    var inventory = [
+        "Candy Bar": Item(price: 12, count: 7),
+        "Chips": Item(price: 10, count: 4),
+        "Pretzels": Item(price: 7, count: 11)
+    ]
+     
+    var coinsDeposited = 0
+    
+    func vend(itemName name: String) throws {
+        
+        guard inventory[name] != nil else {
+            throw VendingMachineError.invalidSelection
+        }
+        
+    }
+}
