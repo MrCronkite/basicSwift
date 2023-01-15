8fd03e1855dac96dea02739a18541a96f9f64638
MrCronkite <vladshima99@gmail.com>
2023-01-15 22:07:07 +0300

    control flow

--- Concurrency.playground/Contents.swift
+++ Concurrency.playground/Contents.swift
@@ -1,13 +1,20 @@
+import Foundation
 
 
 print("hello async")
 
-func listName(inGallery name: String) async -> [String]{
-    await Task.sleep(2 * 1_000_000_000)
-    
-    print("\(name)")
-    
-    return ["vlad", "Josef", "Djo"]
-}
+//func listName(inGallery name: String) async -> [String]{
+//    await Task.sleep(2 * 1_000_000_000)
+//    
+//    print("\(name)")
+//    
+//    return ["vlad", "Josef", "Djo"]
+//}
 
-print("gudbay async\(await listName(inGallery: "Liza"))")
+//print("gudbay async\(await listName(inGallery: "Liza"))")
+
+
+let handle = FileHandle.standardInput
+for try await line in handle.bytes.lines {
+    print(line)
+}
