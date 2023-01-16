30f32bc711dbd6f18e2097de0a3cd39ffeac8a00
MrCronkite <vladshima99@gmail.com>
2023-01-16 17:23:13 +0300

    control flow

--- Type Casting.playground/Contents.swift
+++ Type Casting.playground/Contents.swift
@@ -54,4 +54,23 @@ struct BlackjackCard {
         case spades = "♠", hearts = "♡", diamonds = "♢", clubs = "♣"
     }
     
+    enum Rank: Int {
+        case two = 2, three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king, ace
+        struct Values {
+            let first: Int, second: Int?
+        }
+        
+        var values: Values {
+            switch self {
+            case .ace:
+                return Values(first: 1, second: 11)
+            case .jack, .queen, .king:
+                return Values(first: 10, second: nil)
+            default:
+                return Values(first: self.rawValue, second: nil)
+            }
+        }
+    }
+    
 }
