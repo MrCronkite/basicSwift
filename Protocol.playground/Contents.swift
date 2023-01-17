ef64bde93f50900598f51dd2b0d2b45508deeeca
MrCronkite <vladshima99@gmail.com>
2023-01-17 20:52:30 +0300

    control flow

--- Protocol.playground/Contents.swift
+++ Protocol.playground/Contents.swift
@@ -10,5 +10,21 @@ struct SomeStructura: SomeProtocol {
     func printHello() {
         print("hello world")
     }
+}
+
+protocol FullyNammed {
+    var fullName: String { get }
+}
+
+class Name: FullyNammed {
+    var firstName: String
+    var surname: String
+    init(firstName: String, surname: String){
+        self.firstName = firstName
+        self.surname = surname
+    }
     
+    var fullName: String {
+        return "\(firstName) \(surname)"
+    }
 }
