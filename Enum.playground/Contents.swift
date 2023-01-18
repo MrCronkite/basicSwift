450cdd1779e4285e6949666efd9e0a422125a5a7
MrCronkite <vladshima99@gmail.com>
2023-01-18 22:50:43 +0300

    control flow

--- Enum.playground/Contents.swift
+++ Enum.playground/Contents.swift
@@ -40,3 +40,16 @@ indirect enum Arithmetic{
 var number_1 = Arithmetic.number(12)
 var number_2 = Arithmetic.number(6)
 var multi = Arithmetic.anotherCase(number_1, number_2)
+
+
+enum NewPlanet: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+    
+    static subscript (n: Int) -> NewPlanet {
+        return NewPlanet(rawValue: n)!
+    }
+}
+
+
+var newPlanet = NewPlanet[6]
+print(newPlanet)
