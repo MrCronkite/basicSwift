768ff0b19791d9c7ac08b6c170f2648cf3bc2c08
MrCronkite <vladshima99@gmail.com>
2023-01-22 22:46:00 +0300

    user default

--- UserDefaults/UserDefaults/ViewController.swift
+++ UserDefaults/UserDefaults/ViewController.swift
@@ -23,14 +23,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         saveCurentLaunch()
-        statusLabel.text = "Запусков приложений \(UserDefaults.standard.integer(forKey: "NumberOfLaunches"))"
+        statusLabel.text = "Запусков приложений \(UserDefaults.standard.integer(forKey: numberOfLaunch))"
+        textField.text = UserDefaults.standard.string(forKey: inputText)
+        segmentControl.selectedSegmentIndex = UserDefaults.standard.integer(forKey: segmentIndex)
     }
     
+    @IBAction func saveButton(_ sender: Any) {
+        guard let text = textField.text else {return}
+        UserDefaults.standard.set(text, forKey: inputText)
+        
+    }
+    
+    
+    @IBAction func deleteButton(_ sender: Any) {
+    }
+    
+    @IBAction func segmentState(_ sender: UISegmentedControl) {
+        UserDefaults.standard.set(sender.selectedSegmentIndex, forKey: segmentIndex)
+    }
     
     func saveCurentLaunch() {
-        let save = UserDefaults.standard.integer(forKey: "NumberOfLaunches")
-        UserDefaults.standard.set(save + 1, forKey: "NumberOfLaunches")
+        let save = UserDefaults.standard.integer(forKey: numberOfLaunch)
+        UserDefaults.standard.set(save + 1, forKey: numberOfLaunch)
     }
+    
+    
+    //MARK: - Private Key
+    private let numberOfLaunch = "NumberOfLaunches"
+    private let inputText = "TextKeyInput"
+    private let segmentIndex = "SegmentKey"
 
 }
 
