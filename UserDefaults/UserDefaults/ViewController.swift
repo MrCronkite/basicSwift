ad871d1a82032ba696df9df0697a561fc3644699
MrCronkite <vladshima99@gmail.com>
2023-01-22 22:16:20 +0300

    user default

---
+++ UserDefaults/UserDefaults/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  UserDefaults
+//
+//  Created by admin1 on 22.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var buttonDelete: UIButton!
+    @IBOutlet weak var buttonSave: UIButton!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
