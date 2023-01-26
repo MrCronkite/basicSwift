547a6e219e8dcb77d39d1102a00f8521a797ceca
MrCronkite <vladshima99@gmail.com>
2023-01-26 21:00:31 +0300

    URLSession

--- URLSession/URLSession/API/services/ApiManager.swift
+++ URLSession/URLSession/API/services/ApiManager.swift
@@ -28,5 +28,35 @@ enum ApiType {
         }
     }
     
+    var path: String {
+        switch self {
+        case .login: return "login"
+        case .getUsers: return "users"
+        case .getPosts: return "posts"
+        case .getAlbums: return "albums"
+        }
+    }
     
+    var request: URLRequest {
+        let url = URL(string: path, relativeTo: URL(string: baseUrl)!)!
+        var request = URLRequest(url: url)
+        request.allHTTPHeaderFields = headers
+        switch self {
+        case .login:
+            request.httpMethod = "POST"
+            return request
+        default:
+            request.httpMethod = "GET"
+            return request
+        }
+        
+        
+    }
+    
+}
+
+
+class ApiManager {
+    
+    static let shared = ApiManager()
 }
