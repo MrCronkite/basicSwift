a1f582b475742da31ce8c6975f9f014e35799513
MrCronkite <vladshima99@gmail.com>
2023-01-27 21:24:22 +0300

    URLSession

--- CONCURRENT.playground/Contents.swift
+++ CONCURRENT.playground/Contents.swift
@@ -10,3 +10,15 @@ let utilityQueue = DispatchQueue.global(qos: .utility)
 let backgroundQueue = DispatchQueue.global(qos: .background)
 
 let defаultQueue = DispatchQueue.global()
+
+
+func task(_ symbol: String) { for i in 1...10 {
+    print("\(symbol) \(i) prioritet = \(qos_class_self().rawValue)")
+    }
+}
+
+func taskHIGH(_ symbol: String) {
+    print("\(symbol) HIGH prioritet = \(qos_class_self().rawValue)")
+}
+
+
