fbf2ac0db6b2e6594e372d7d4db196d26a0afd2d
MrCronkite <vladshima99@gmail.com>
2023-01-31 23:30:03 +0300

    life circle

--- lifeCycle.playground/Contents.swift
+++ lifeCycle.playground/Contents.swift
@@ -7,5 +7,12 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 var view = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
 view.backgroundColor = .gray
 
+func viewDidLoad(){
+    
+    print("hello world")
+    
+}
+
+
 PlaygroundPage.current.liveView = view
 
