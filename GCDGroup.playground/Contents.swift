933647ab5034be78bd0a32fd47051a32076638de
MrCronkite <vladshima99@gmail.com>
2023-02-09 23:15:54 +0300

    1.2.1

--- GCDGroup.playground/Contents.swift
+++ GCDGroup.playground/Contents.swift
@@ -28,7 +28,15 @@ public class EightImages: UIView{
     }
 }
 
-
-
 PlaygroundPage.current.needsIndefiniteExecution = true
+
+var view = EightImages(frame: CGRect(x: 0, y: 0, width: 200, height: 500))
+view.backgroundColor = UIColor.red
+
+let imageURLs = ["http://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg", "http://adriatic-lines.com/wp-content/uploads/2015/04/canal-of-Venice.jpg", "http://bestkora.com/IosDeveloper/wp-content/uploads/2016/12/Screen-Shot-2017-01-17-at-9.33.52-PM.png", "http://www.picture-newsletter.com/arctic/arctic-12.jpg" ]
+var images = [UIImage] ()
+
+PlaygroundPage.current.liveView = view
+
+
 
