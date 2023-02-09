5df0d9356229721892d8bc34a8b0db1a176abe6f
MrCronkite <vladshima99@gmail.com>
2023-02-09 23:13:37 +0300

    1.2.1

---
+++ GCDGroup.playground/Contents.swift
@@ -0,0 +1,34 @@
+
+import UIKit
+import PlaygroundSupport
+
+public class EightImages: UIView{
+    public var ivs = [UIImageView] ()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        ivs.append(UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100)))
+        ivs.append(UIImageView(frame: CGRect(x: 0, y: 100, width: 100, height: 100)))
+             ivs.append(UIImageView(frame: CGRect(x: 100, y: 0, width: 100, height: 100)))
+             ivs.append(UIImageView(frame: CGRect(x: 100, y: 100, width: 100, height: 100)))
+             
+             ivs.append(UIImageView(frame: CGRect(x: 0, y: 300, width: 100, height: 100)))
+             ivs.append(UIImageView(frame: CGRect(x: 100, y: 300, width: 100, height: 100)))
+             ivs.append(UIImageView(frame: CGRect(x: 0, y: 400, width: 100, height: 100)))
+             ivs.append(UIImageView(frame: CGRect(x: 100, y: 400, width: 100, height: 100)))
+        for i in 0...7 {
+            ivs[i].contentMode = .scaleAspectFit
+            self.addSubview(ivs[i])
+        }
+    }
+    
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
