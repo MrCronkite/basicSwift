015d9ec4b4b9e0c8bb6d77446179ff8be0a99e32
MrCronkite <vladshima99@gmail.com>
2023-02-12 20:45:44 +0300

    pods instal

---
+++ SnapKit/SnapKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapKit
+//
+//  Created by admin1 on 12.02.23.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    private func initi
+}
+
