757e47adfc07a1ed56aa0aae77de5e1d14544ca8
MrCronkite <vladshima99@gmail.com>
2023-02-13 22:40:38 +0300

    pods instal

--- SnapKit/SnapKit/AccountView.swift
+++ SnapKit/SnapKit/AccountView.swift
@@ -19,6 +19,12 @@ class AccountView: UIView {
         initialize()
     }
     
+    //MARK: - Private constants
+    private enum UIConstants {
+        static let cardWidth: CGFloat = 45
+        static let cardHeight: CGFloat = 30
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -39,11 +45,42 @@ class AccountView: UIView {
     }()
     
     private var collectionView: UICollectionView!
+    private var cards: [CardThumbnailInfo] = []
 }
 
 
 private extension AccountView {
     func initialize() {
         backgroundColor = .clear
+        
+        let yStack = UIStackView()
+        yStack.axis = .vertical
+        yStack.addArrangedSubview(amountLabel)
+        yStack.addArrangedSubview(accountNameLabel)
+        
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        yStack.addArrangedSubview(collectionView)
+    }
+}
+
+
+extension AccountView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        cards.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+}
+
+
+extension AccountView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: UIConstants.cardWidth, height: UIConstants.cardHeight)
     }
 }
---
+++ SnapKit/SnapKit/AccountViewCardCell.swift
@@ -0,0 +1,44 @@
+//
+//  AccountViewCardCell.swift
+//  SnapKitProgect
+//
+//  Created by admin1 on 13.02.23.
+//
+
+import SnapKit
+import UIKit
+
+class AccountViewCardCell: UICollectionViewCell {
+    //MARK: - Public
+    func configure(with image: UIImage) {
+        imageView.image = image
+    }
+    
+    
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Private properties
+    private let imageView: UIImageView = {
+        let view = UIImageView()
+        return view
+    }()
+}
+
+
+//MARK: - Private methods
+private extension AccountViewCardCell {
+    func initialize() {
+        contentView.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
