0103301082e881a91d13ac53c640ff7ec7af0d26
MrCronkite <vladshima99@gmail.com>
2023-05-03 21:27:35 +0300

    add new arch

---
+++ VIPER/VIPER/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  VIPER
+//
+//  Created by admin1 on 3.05.23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+                window?.windowScene = windowScene
+                window?.rootViewController = ViewController()
+                window?.makeKeyAndVisible()
+    }
+}
+
---
+++ VIPER/VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER
+//
+//  Created by admin1 on 3.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
