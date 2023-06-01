1eb53fc8b2357e07f953c6878e5509c6df69466a
MrCronkite <vladshima99@gmail.com>
2023-06-01 22:08:19 +0300

    add new file

--- Questions/Questions/6.swift
+++ Questions/Questions/6.swift
@@ -14,6 +14,9 @@ import Foundation
   Copy-on-write — механизм оптимизации в Swift, когда при присвоении переменной значений или
   при передаче коллекции в функцию не происходит копирование этой коллекции.
   
-  Подробнее о механизме Copy-on-wirite написаны в статье: https://ios-interview.ru/copy-on-write/
+  
 
   */
+
+//Подробнее о механизме Copy-on-wirite написаны в статье:
+https://ios-interview.ru/copy-on-write/
