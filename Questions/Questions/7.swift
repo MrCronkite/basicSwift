06e47ba35e1359b976462a5d37864f8435a782ac
MrCronkite <vladshima99@gmail.com>
2023-06-02 19:28:12 +0300

    add new file

--- Questions/Questions/7.swift
+++ Questions/Questions/7.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+protocol SomeProtocol {
+    // определение протокола SomeProtocol
+}
+
+protocol AnotherProtocol {
+    // определение протокола AnotherProtocol
+}
+
+protocol InheritingProtocol: SomeProtocol, AnotherProtocol {
+    // определение протокола InheritingProtocol
+}
+
+/*
+Протокол может наследовать один или более других протоколов. Также, как и класс,
+протокол добавляет требования поверх тех требований протоколов, которые наследует.
+
+В нашем примере протокол InheritingProtocol должен удовлетворять всем требованиям
+протоколов SomeProtocol и AnotherProtocol.
+*/
+
+Дополнительное чтение: https://swiftbook.ru/content/languageguide/protocols/
