8642331e767f1e8efa64e8483210a7b045e7428c
MrCronkite <vladshima99@gmail.com>
2023-07-07 21:45:07 +0300

    refactoring

--- clusureTask.playground/Contents.swift
+++ clusureTask.playground/Contents.swift
@@ -1,55 +1,19 @@
+let numbers: [Int] = [1, 2, 5, 6, 8]
 
-
-enum Compas: CaseIterable {
-    case north
-    case south
-    case east
-    case west
+func reversArrey(arr: [Int]) -> [Int] {
+    var squareNumbers: [Int] = []
+    arr.map { squareNumbers.insert($0, at: 0) }
+    return squareNumbers
 }
 
-var derectToHead = Compas.west
+let newArrey = reversArrey(arr: numbers)
 
-derectToHead = .north
-
-switch derectToHead {
-case .east:
-    print("east")
-case .north:
-    print("north")
-case .south:
-    print("south")
-case .west:
-    print("west")
+func sumNumbers(arrey: [Int]) -> Int {
+    var number = 0
+    arrey.map { if $0 % 2 == 0 { number += $0 } }
+    return number
 }
 
-print(Compas.allCases.count)
+sumNumbers(arrey: numbers)
 
-enum Planet: Int, CaseIterable {
-    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
-}
-
-for planet in Planet.allCases {
-    print(planet.rawValue)
-}
-
-indirect enum Arithmetic{
-    case number(Int)
-    case anotherCase(Arithmetic, Arithmetic)
-}
-
-var number_1 = Arithmetic.number(12)
-var number_2 = Arithmetic.number(6)
-var multi = Arithmetic.anotherCase(number_1, number_2)
-
-
-enum NewPlanet: Int {
-    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
-    
-    static subscript (n: Int) -> NewPlanet {
-        return NewPlanet(rawValue: n)!
-    }
-}
-
-
-var newPlanet = NewPlanet[6]
-print(newPlanet)
+print(newArrey)
