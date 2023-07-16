2b15807909681c8996cf5e242e066d8c12e843a2
MrCronkite <vladshima99@gmail.com>
2023-07-16 20:18:06 +0300

    refactoring

--- Keychain/Keychain/Storage.swift
+++ Keychain/Keychain/Storage.swift
@@ -9,7 +9,57 @@ import Security
 import Foundation
 
 protocol Keychain {
-    func save() throws
-    func load() throws -> Data?
-    func delete() throws
+    func save(key: KeychainImpl.Keys, data: Data) throws
+    func load(key: KeychainImpl.Keys) throws -> Data?
+    func delete(key: KeychainImpl.Keys) throws
+}
+
+final class KeychainImpl: Keychain {
+    public enum Keys: String {
+        case jeyData
+    }
+    
+    private enum KeychainError: Error {
+            case saveFailed
+            case loadFailed
+            case deleteFailed
+    }
+    
+    func save(key: Keys, data: Data) throws {
+        let query = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecValueData: data
+        ] as [String: Any]
+        
+        let status = SecItemAdd(query as CFDictionary, nil)
+        if status != errSecSuccess {
+            throw KeychainError.saveFailed
+        }
+    }
+    
+    func load(key: Keys) throws -> Data? {
+        let query = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecReturnData: kCFBooleanTrue!,
+            kSecMatchLimit: kSecMatchLimitOne
+        ] as [String: Any]
+        
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+        if status == errSecSuccess {
+            return result as? Data
+        } else if status == errSecItemNotFound {
+            return nil
+        } else {
+            throw KeychainError.loadFailed
+        }
+    }
+    
+    func delete(key: Keys) throws {
+        <#code#>
+    }
+    
+    
 }
