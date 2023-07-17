dc8eeccc974c0388bd94a421be4f58ef63295369
MrCronkite <vladshima99@gmail.com>
2023-07-17 20:44:04 +0300

    refactoring

--- Concurrency/Concurrency.playground/Contents.swift
+++ Concurrency/Concurrency.playground/Contents.swift
@@ -1,20 +1,34 @@
 import Foundation
 
-
-print("hello async")
-
-//func listName(inGallery name: String) async -> [String]{
-//    await Task.sleep(2 * 1_000_000_000)
-//    
-//    print("\(name)")
-//    
-//    return ["vlad", "Josef", "Djo"]
-//}
-
-//print("gudbay async\(await listName(inGallery: "Liza"))")
-
-
-let handle = FileHandle.standardInput
-for try await line in handle.bytes.lines {
-    print(line)
+func fetchDataFromServer(completion: @escaping (Result<String, Error>) -> Void) {
+    // Пример асинхронной операции, которая занимает время
+    DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
+        if Bool.random() {
+            completion(.success("Данные успешно получены"))
+        } else {
+            completion(.failure(NSError(domain: "com.example", code: 0, userInfo: [NSLocalizedDescriptionKey: "Ошибка получения данных"])))
+        }
+    }
 }
+
+func processFetchedData(data: String) {
+    print("Обработка полученных данных: \(data)")
+}
+
+async func fetchDataAndProcess() {
+    do {
+        let data = try await withUnsafeThrowingContinuation { (continuation: UnsafeContinuation<Result<String, Error>, Never>) in
+            fetchDataFromServer { result in
+                continuation.resume(with: result)
+            }
+        }
+        processFetchedData(data: data)
+    } catch {
+        print("Ошибка: \(error)")
+    }
+}
+
+Task {
+    await fetchDataAndProcess()
+}
+
