d9c09edd8a40477a9c76d61fa9f7ff9f14f2123d
Влад Шимченко <vladshima99@gmail.com>
2026-05-10 22:12:15 +0300

    continuation

--- Await/Await/ViewController.swift
+++ Await/Await/ViewController.swift
@@ -19,12 +19,15 @@ struct BarResult {
 
 class ViewController: UIViewController {
 
+    private let network = Network()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         Task {
-            let value =  await getAsyncValue()
             await qux()
+            let coins = await network.getCoins()
+            coins.forEach { print($0.name) }
         }
 
     }
@@ -48,9 +51,53 @@ class ViewController: UIViewController {
     }
 
     func doSomething(_ foo: FooResult, _ bar: BarResult) {
-        print(foo)
-        print(bar)
+        print(foo.result)
+        print(bar.result)
     }
 
+    func getAsyncV() async {
+        var x = 0
+
+        for x in 0..<1000000000 {
+            await Task.yield()
+        }
+    }
+
+}
+
+final class Network {
+
+    private enum API {
+        static let url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
+    }
+
+    private let urlSession: URLSession
+    private let jsonDecoder: JSONDecoder
+
+    init(
+        urlSession: URLSession = .shared,
+        jsonDecoder: JSONDecoder = JSONDecoder()
+    ) {
+        self.urlSession = urlSession
+        self.jsonDecoder = jsonDecoder
+    }
+
+    func getCoins() async -> [Coin] {
+        return await withCheckedContinuation { continuation in
+            urlSession.dataTask(
+                with: URLRequest(url: URL(string: API.url)!)
+            ) { [weak self] data, response, error in
+                guard let data, let self else { return }
+
+                let result: [Coin] = try! self.jsonDecoder.decode([Coin].self, from: data)
+                continuation.resume(returning: result)
+            }.resume()
+        }
+    }
+}
+
+struct Coin: Decodable {
+    let id: String
+    let name: String
 }
 
