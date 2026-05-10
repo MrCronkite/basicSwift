12f19dfbe41004b005c27cd280b17291ce9db89b
Влад Шимченко <vladshima99@gmail.com>
2026-05-10 21:39:42 +0300

    Executers

--- Await/Await/ViewController.swift
+++ Await/Await/ViewController.swift
@@ -7,20 +7,49 @@
 
 import UIKit
 
+struct FooResult {
+    let result: String
+}
+
+struct BarResult {
+    let result: Int
+}
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         Task {
-            let value = await getAsyncValue()
-            print(value)
+            let value =  await getAsyncValue()
+            await qux()
         }
 
     }
 
     func getAsyncValue() async -> String {
         "Hello"
+    }
+
+    func foo() async -> FooResult {
+        FooResult(result: "Hello")
+    }
+
+    func bar() async -> BarResult {
+        BarResult(result: 23)
+    }
+
+    func qux() async {
+        let foo = await foo()
+        let bar = await bar()
+        doSomething(foo, bar)
+    }
+
+    func doSomething(_ foo: FooResult, _ bar: BarResult) {
+        print(foo)
+        print(bar)
     }
 
 }
